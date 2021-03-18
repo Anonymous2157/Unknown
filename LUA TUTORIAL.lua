91b8b1e31ee6e18d40b54f7a5d83630c816808fa
Glitcher64 <80603741+Anonymous2157@users.noreply.github.com>
2021-03-18 15:38:32 -0400

    LUATUTORIAL.lua
    Here is a small tutorial on how to use Lua for math!

---
+++ LUA TUTORIAL.lua
@@ -0,0 +1,25 @@
+# README.md
+# Hi! This is a tutorial for how to code with lua! Let us use lua for math.
+
+lua> 2 + 2 
+# This command shows the answer to 2 + 2, which is 4!
+# You can do this with other numbers. Let me give you another example!
+  lua> 44 + 505
+  
+  # This will also tell you the answer!
+  # But you can also use it for other equations.
+  
+  lua> 500 * 500
+  
+  # The Sign for Multiplication is different, instead of a X. It is a asterisk's.
+  # Let us say that you want to use Parantheses and Order of Operation for Lua.
+  
+  lua> (224 * 3) + 6 - 33
+  
+  # You can do that too. You can do the same thing without parantheses.
+      
+      lua> 224 * 3 + 6 - 33
+      
+      # What, we have done is we used Lua for math! Lua is a program that can be used for math, but that is not all of the features of Lua. We only have done a little.
+      # I will see you for Lua Again!
+
